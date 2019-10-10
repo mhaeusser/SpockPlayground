af398fe2c68cf3062f548867850c724a45fcf5e2
Matthias Häußer <Matthias.Haeusser@EXXETA.com>
2019-10-10 23:18:40 +0200

    Add test classes

---
+++ src/test/groovy/FirstSpecification.groovy
@@ -0,0 +1,21 @@
+import spock.lang.Specification
+
+class FirstSpecification extends Specification {
+
+    def "one plus one should equal two"() {
+        expect:
+        1 + 1 == 2
+    }
+
+    def "two plus two should equal four"() {
+        given:
+        int left = 2
+        int right = 2
+
+        when:
+        int result = left + right
+
+        then:
+        result == 4
+    }
+}
